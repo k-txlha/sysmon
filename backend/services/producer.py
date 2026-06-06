6484f06cc2b875dce4ac7656a953b7a78f465178
talhakhan7640 <talhakhan7640@gmail.com>
2026-06-06 22:59:45 +0530

    feat: add kafka producer service

--- backend/services/producer.py
+++ backend/services/producer.py
@@ -1,3 +1,39 @@
 from utils.logger import setup_logger
+from aiokafka import AIOKafkaProducer
+import asyncio
+from config import settings
+import json
 
 logger = setup_logger("producer")
+
+
+class KafkaProducerService:
+    def __init__(self) -> None:
+        self.producer = None
+
+    async def start_service(self):
+        self.producer = AIOKafkaProducer(
+            bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVERS,
+            acks="all",
+            compressio_type="gzip",
+            enable_idempotence=True,
+            batch_size=65536,
+            linger_ms=500,
+        )
+        await self.producer.start()
+        logger.info("Kafka broker is ready to consume records")
+
+    async def stop_service(self):
+        if self.producer:
+            await self.producer.stop()
+            logger.info("Kafka broker stopped!")
+
+    async def stream_data(self, topic: str, data: dict):
+        if not self.producer:
+            raise RuntimeError("Kafka producer is not initialized")
+
+        payload_bytes = json.dumps(data).encode("utf-8")
+        await self.producer.send_and_wait(topic, payload_bytes)
+
+
+kafka_service = KafkaProducerService()
