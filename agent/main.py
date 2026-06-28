b0d583898f4f2f0fc7e240e48b262eab8ce800e7
talhakhan7640 <talhakhan7640@gmail.com>
2026-06-28 19:20:36 +0530

    fixed indentation

--- agent/main.py
+++ agent/main.py
@@ -1,3 +1,4 @@
+import json
 import time
 from utils.logger import setup_logger
 from utils.assembler import assemble_telemetry_payload
@@ -6,22 +7,26 @@ from utils.config import settings
 
 logger = setup_logger("main")
 
+
 def main():
     logger.info("Initializing SIEM Agent...")
     # Read the interval from settings
-    logger.info(f"Agent loop scheduled to run every {settings.COLLECTION_INTERVAL} seconds.")
+    logger.info(
+        f"Agent loop scheduled to run every {settings.COLLECTION_INTERVAL} seconds."
+    )
     print("-" * 60)
-    
+
     try:
         while True:
             payload = assemble_telemetry_payload()
             ship_to_backend(payload)
-            
+
             # Use configuration value for sleep timer
             time.sleep(settings.COLLECTION_INTERVAL)
-            
+
     except KeyboardInterrupt:
         logger.info("Agent gracefully stopped by user interaction.")
+
 
 if __name__ == "__main__":
     main()
